861f6a07e8ad6a11b8d0a33fc421a4fbbc9b7c17
Lipson <liipson@wp.pl>
2022-04-14 20:08:16 +0200

    zmienne

--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -3,9 +3,31 @@ package com.company;
 public class MyClass
 {
     //2 pola typu całkowite (jedno dostępne dla wszystkich, drugie tylko w tej klasie)
-    public int calkowita1;
-    private int calkowita2;
-    
+     public int calkowita1;
+     private int calkowita2;
+    //1 pole typu rzeczywistego (dostępne tylko w tej klasie)
+     private float rzeczywista1;
+    //1 pole przetrzymujące wartość logiczną (dostępne tylko w tej klasie, jej domyślna wartość powinna być ustawiona na true)
+     private boolean logiczna1=true;
+    //1 pole przetrzymujące znak(dostępny tylko w tej klasie)
+     private char znak1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
