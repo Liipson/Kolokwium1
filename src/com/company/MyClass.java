f9aea100979c5a96f9bfee8d41beae0edb303357
Lipson <liipson@wp.pl>
2022-04-14 20:00:17 +0200

    Initial commit

---
+++ src/com/company/MyClass.java
@@ -0,0 +1,11 @@
+//napisz publiczną klasę o nazwie MyClass
+package com.company;
+public class MyClass
+{
+    //2 pola typu całkowite (jedno dostępne dla wszystkich, drugie tylko w tej klasie)
+    public int calkowita1;
+    private int calkowita2;
+    
+
+
+}
